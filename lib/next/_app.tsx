0eae6efd2a3c50d66eca2e8d1e4699b8eecac83a
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-06-23 12:36:50 +0200

    feat(next): enhanced Next setup

--- lib/next/_app.tsx
+++ lib/next/_app.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 
+declare module 'react' {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean;
+    global?: boolean;
+  }
+}
+
+const Layout = ({ children = {}, ...otherProps }) => (
+  <main {...otherProps}>
+    {children}
+    <style jsx global>{`
+      body {
+        font-family: 'Open Sans';
+        font-size: 16px;
+      }
+    `}</style>
+  </main>
+);
+
 export default class Unicat extends App {
   static async getInitialProps({ Component, ctx }) {
     if (Component.getInitialProps) {
@@ -15,7 +34,9 @@ export default class Unicat extends App {
 
     return (
       <Container>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </Container>
     );
   }
