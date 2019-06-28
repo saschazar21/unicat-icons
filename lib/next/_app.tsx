3d3e00946f00fb9982f0ff1ac454172994c90a1a
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-06-28 14:45:05 +0200

    feat(docs): added footer to template

--- lib/next/_app.tsx
+++ lib/next/_app.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import App, { Container } from "next/app";
+import React, { Fragment } from 'react';
+import App, { Container } from 'next/app';
+import Link from 'next/link';
+
+import pkg from '../package.json';
+import paths from './paths.json';
 
 import 'highlight.js/styles/a11y-dark.css';
 
-declare module "react" {
+declare module 'react' {
   interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
     jsx?: boolean;
     global?: boolean;
@@ -11,102 +15,183 @@ declare module "react" {
 }
 
 const Layout = ({ children = {}, ...otherProps }) => (
-  <main {...otherProps}>
-    {children}
-    <style jsx global>{`
-      html {
-        font-size: 16px;
-      }
+  <Fragment>
+    <main {...otherProps}>
+      {children}
+      <style jsx global>{`
+        html {
+          font-size: 16px;
+        }
 
-      body {
-        background-color: white;
-        font-family: "Open Sans", sans-serif;
-        font-weight: 400;
-        line-height: 1.45;
-        color: #333;
-      }
+        body {
+          background-color: white;
+          font-family: 'Open Sans', sans-serif;
+          font-weight: 400;
+          line-height: 1.45;
+          color: #333;
+          margin: 0;
+        }
 
-      p {
-        margin-bottom: 1.25em;
-      }
+        p {
+          margin-bottom: 1.25em;
+        }
 
-      h1,
-      h2,
-      h3,
-      h4,
-      h5 {
-        margin: 2.75rem 0 1rem;
-        font-family: "Open Sans", sans-serif;
-        font-weight: 400;
-        line-height: 1.15;
-      }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5 {
+          margin: 2.75rem 0 1rem;
+          font-family: 'Open Sans', sans-serif;
+          font-weight: 400;
+          line-height: 1.15;
+        }
 
-      h1 {
-        margin-top: 0;
-        font-size: 3.052em;
-      }
+        h1 {
+          margin-top: 0;
+          font-size: 3.052em;
+        }
 
-      h2 {
-        font-size: 2.441em;
-      }
+        h2 {
+          font-size: 2.441em;
+        }
 
-      h3 {
-        font-size: 1.953em;
-      }
+        h3 {
+          font-size: 1.953em;
+        }
 
-      h4 {
-        font-size: 1.563em;
-      }
+        h4 {
+          font-size: 1.563em;
+        }
 
-      h5 {
-        font-size: 1.25em;
-      }
+        h5 {
+          font-size: 1.25em;
+        }
 
-      small,
-      code,
-      .text_small {
-        font-size: 0.8em;
-      }
+        small,
+        code,
+        .text_small {
+          font-size: 0.8em;
+        }
 
-      code {
-        -webkit-font-smoothing: antialiased;
-        display: inline-block;
-        font-family: "Fira Mono", monospace;
-        border-radius: 4px;
-        background-color: #2b2b2b;
-        color: #f8f8f2;
-        padding: 0.5em;
-        margin: 0.25em;
-      }
+        code {
+          -webkit-font-smoothing: antialiased;
+          display: inline-block;
+          font-family: 'Fira Mono', monospace;
+          border-radius: 4px;
+          background-color: #2b2b2b;
+          color: #f8f8f8;
+          padding: 0.5em;
+          margin: 0.25em;
+        }
 
-      pre {
-        border-radius: 4px;
-        background-color: #2b2b2b;
-        padding: 1.25em;
-        max-width: 100%;
-        overflow: auto;
-      }
+        pre {
+          border-radius: 4px;
+          background-color: #2b2b2b;
+          padding: 1.25em;
+          max-width: 100%;
+          overflow: auto;
+        }
 
-      blockquote {
-        background-color: #f8f8f8;
-        border-left: 8px solid darkgrey;
-        padding: 1.25em;
-        margin: 1em 0 2em 0;
-        line-height: 1.45em;
-      }
+        blockquote {
+          background-color: #f8f8f8;
+          border-left: 8px solid darkgrey;
+          padding: 1.25em;
+          margin: 1em 0 2em 0;
+          line-height: 1.45em;
+        }
 
-      blockquote > * {
-        margin: 0;
-      }
-    `}</style>
-    <style jsx>{`
-      main {
-        padding: 2em;
-        margin: 0 auto;
-        max-width: 840px;
-      }
-    `}</style>
-  </main>
+        blockquote > * {
+          margin: 0;
+        }
+      `}</style>
+      <style jsx>{`
+        main {
+          padding: 2em;
+          margin: 0 auto;
+          max-width: 840px;
+        }
+      `}</style>
+    </main>
+    <footer>
+      <nav aria-labelledby="footer-navigation">
+        <strong id="footer-navigation">Navigation</strong>
+        {paths && paths.length && (
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Index</a>
+              </Link>
+            </li>
+            {paths.map((p, i) => (
+              <li key={`link-${p}-${i}`}>
+                <Link href={`/${p}`}>
+                  <a>{p}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </nav>
+      <strong className="description">
+        {pkg.name} - v{pkg.version}
+      </strong>
+      <style jsx>{`
+        footer {
+          background: #2b2b2b;
+          color: #f8f8f8;
+          border-top: 4px solid darkgrey;
+          padding: 2em;
+        }
+
+        nav {
+          display: flex;
+          flex-direction: column;
+          line-height: 2em;
+          max-width: 840px;
+          margin: 0 auto;
+        }
+
+        ul {
+          list-style-type: none;
+          padding-left: 0;
+          margin: 0;
+        }
+
+        a {
+          color: inherit;
+        }
+
+        a:hover,
+        a:focus,
+        a:visited {
+          color: lightgrey;
+        }
+
+        .description {
+          display: block;
+          text-align: center;
+          margin: 2em 1em auto;
+        }
+
+        @media screen and (min-width: 768px) {
+          nav {
+            flex-direction: row;
+          }
+
+          ul {
+            display: flex;
+            flex-wrap: wrap;
+          }
+
+          strong,
+          li {
+            margin-right: 1em;
+          }
+        }
+      `}</style>
+    </footer>
+  </Fragment>
 );
 
 export default class Unicat extends App {
